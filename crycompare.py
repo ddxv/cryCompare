885d3dd483ee57daddf223655a95fca2f57eaf01
stefs304 <30340665+stefs304@users.noreply.github.com>
2017-07-21 14:11:44 +0200

    New methods added

--- crycompare.py
+++ crycompare.py
@@ -12,7 +12,9 @@ class Price:
 		self.pricemultifullurl = 'https://min-api.cryptocompare.com/data/pricemultifull?'
 		self.generateavgurl = 'https://min-api.cryptocompare.com/data/generateAvg?'
 		self.dayavgurl = 'https://min-api.cryptocompare.com/data/dayAvg?'
-		self.pricehisturl = ''
+		self.historicalurl = 'https://min-api.cryptocompare.com/data/pricehistorical?'
+		self.coinsnapshoturl = 'https://www.cryptocompare.com/api/data/coinsnapshot/?'
+		self.coinsnapshotfull = 'https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?'
 
 	def coin_list(self):
 		return self.get_url(self.coinlisturl)
@@ -26,6 +28,10 @@ class Price:
 	def pricemultifull(self, from_curr, to_curr, e=None, extraParams=None, sign=False, tryConversion=True):
 		return self.get_price(self.pricemultifullurl, from_curr, to_curr, e, extraParams, sign, tryConversion)
 
+	def priceHistorical(self, from_curr, to_curr, markets, ts=None, e=None, extraParams=None,
+						sign=False, tryConversion=True):
+		return self.get_price(self.historicalurl, from_curr, to_curr, markets, e, extraParams, sign, tryConversion)
+
 	def generateAvg(self, from_curr, to_curr, markets, extraParams=None, sign=False, tryConversion=True):
 		return self.get_avg(self.generateavgurl, from_curr, to_curr, markets, extraParams, sign, tryConversion)
 
@@ -34,17 +40,25 @@ class Price:
 		return self.get_avg(self.dayavgurl, from_curr, to_curr, e, extraParams, sign,
 							tryConversion, avgType, UTCHourDiff, toTs)
 
+	def coinSnapshot(self, from_curr, to_curr):
+		return self.get_url(self.coinsnapshoturl + 'fsym=' + from_curr.upper() + '&tsym=' + to_curr.upper())
+
+	def coinSnapshotFullById(self, coin_id):
+		return self.get_url(self.coinsnapshotfull + 'id=' + str(coin_id))
+
 	def stream(self, target, args, interval=60, filepath=None):
 		def generate(f, a, i):
 			while 1:
 				yield f(*a)
 				time.sleep(i)
+
 		# if isinstance(filepath, str):
 		for k in generate(target, args, interval):
 			k['timestamp'] = time.time()
 			print(k)
 
-	def get_price(self, baseurl, from_curr, to_curr, e=None, extraParams=None, sign=False, tryConversion=True):
+	def get_price(self, baseurl, from_curr, to_curr, e=None, extraParams=None, sign=False,
+				  tryConversion=True, markets=None, ts=None):
 		args = list()
 		if isinstance(from_curr, str):
 			args.append('fsym=' + from_curr.upper())
@@ -54,12 +68,18 @@ class Price:
 			args.append('tsyms=' + ','.join(to_curr).upper())
 		elif isinstance(to_curr, str):
 			args.append('tsyms=' + to_curr.upper())
+		if isinstance(markets, str):
+			args.append('markets=' + markets)
+		elif isinstance(markets, list):
+			args.append('markets=' + ','.join(markets))
 		if e:
 			args.append('e=' + e)
 		if extraParams:
 			args.append('extraParams=' + extraParams)
 		if sign:
 			args.append('sign=true')
+		if ts:
+			args.append('ts=' + str(ts))
 		if not tryConversion:
 			args.append('tryConversion=false')
 		if len(args) >= 2:
@@ -87,7 +107,7 @@ class Price:
 		if avgType:
 			args.append('avgType=' + avgType)
 		if UTCHourDiff:
-			args.append('UTCHourDiff=' + str(UTCHourDiff))
+			args.append('UTCHourDiff=' + UTCHourDiff)
 		if toTs:
 			args.append('toTs=' + toTs)
 		if not tryConversion:
